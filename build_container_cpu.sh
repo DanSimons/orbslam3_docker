bba25ba14d97ba024a428a8a5d534f7d9e4841ec
dan <dansimons00@gmail.com>
2024-12-05 13:46:40 -0700

    cpu container changes

--- build_container_cpu.sh
+++ build_container_cpu.sh
@@ -20,7 +20,6 @@ touch $XAUTH
 xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
 
 xhost +local:docker
-
 docker pull jahaniam/orbslam3:ubuntu20_noetic_cpu
 
 # Remove existing container
@@ -28,7 +27,7 @@ docker rm -f orbslam3 &>/dev/null
 [ -d "ORB_SLAM3" ] && sudo rm -rf ORB_SLAM3 && mkdir ~/Packages/ORB_SLAM3
 
 # Create a new container
-docker run -td --privileged --net=host --ipc=host \
+docker run -td  --net=host --ipc=host \
     --name="orbslam3" \
     --memory=8g \
     -e "DISPLAY=$DISPLAY" \
@@ -36,15 +35,16 @@ docker run -td --privileged --net=host --ipc=host \
     -v "/tmp/.X11-unix:/tmp/.X11-unix:rw" \
     -e "XAUTHORITY=$XAUTH" \
     -e ROS_IP=127.0.0.1 \
-    --cap-add=SYS_PTRACE \
     -v `pwd`/Datasets:/Datasets \
     -v /etc/group:/etc/group:ro \
     -v `pwd`/ORB_SLAM3:/root/Packages/ORB_SLAM3 \
+    -v `pwd`/scripts:/root/scripts \
     jahaniam/orbslam3:ubuntu20_noetic_cpu bash
     
 # Git pull orbslam and compile
 docker exec -it orbslam3 bash -i -c  "git clone -b add_euroc_example.sh https://github.com/jahaniam/ORB_SLAM3.git ~/Packages/ORB_SLAM3 && cd ~/Packages/ORB_SLAM3 && chmod +x build.sh && ./build.sh "
 # Compile ORBSLAM3-ROS
 docker exec -it orbslam3 bash -i -c "echo 'ROS_PACKAGE_PATH=/opt/ros/noetic/share:~/Packages/ORB_SLAM3/Examples/ROS'>>~/.bashrc && source ~/.bashrc && cd ~/Packages/ORB_SLAM3 && chmod +x build_ros.sh && ./build_ros.sh"
+docker exec -it orbslam3 bash -i -c "apt-get update && apt-get install -y python3-catkin-tools ros-noetic-camera-calibration && rm -rf /var/lib/apt/lists/* "
 # Git pull orbslam ros wrapper
 docker exec -it orbslam3 bash -i -c "git clone https://github.com/DanSimons/orb_slam3_ros_wrapper.git ~/catkin_ws/src/ && cd ~/catkin_ws && catkin build -j1 -p1 && cp ~/Packages/ORB_SLAM3/Vocabulary/ORBvoc.txt /root/catkin_ws/src/config/ && echo 'source /root/catkin_ws/devel/setup.bash' >> ~/.bashrc"
